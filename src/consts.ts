8dae46c30a5df6836467b47b943d935bc1434b3c
Anil Bridgpal <bridgpal@gmail.com>
2026-02-17 13:33:06 -0500

    Initial notes.anil.computer

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Canonical title for the notes sub-site.
+export const SITE_TITLE = 'notes.anil.computer';
+export const SITE_DESCRIPTION = 'Projects and experiment notes from Anil.';
---
+++ src/content/blog/hello-world/index.md
@@ -0,0 +1,9 @@
+---
+title: "Hello world"
+description: "A short note to test folder slugs + local images."
+pubDate: 2026-02-17
+---
+
+This is the first real note.
+
+![screenshot](/images/hello-world/screenshot.svg)
